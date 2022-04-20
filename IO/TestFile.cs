39b667cb6f4b93acb75cd7121ede1aaa7f6329f6
Etny <ynteveeman@gmail.com>
2022-04-20 22:32:38 +0200

    Ok now it works (I promise)

--- IO/TestFile.cs
+++ IO/TestFile.cs
@@ -20,7 +20,7 @@ namespace DynamicCheck.IO {
             
             using var file = File.Create(FilePath);
             using var template = Assembly.GetExecutingAssembly()
-                                    .GetManifestResourceStream($"Template-{stage.FileName}.txt")
+                                    .GetManifestResourceStream($"Template-{stage.FileName}")
                                     ?? throw new FileNotFoundException($"Failed to find Manifest Resource 'DynamicCheck.{stage.FileName}.txt'");
 
             using var reader = new StreamReader(template);
