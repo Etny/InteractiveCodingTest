9076421b8f9abcd137ccdfdf3b038a0975094c3b
Etny <ynteveeman@gmail.com>
2022-05-30 13:35:17 +0200

    Expanded run.bat

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,2 +1,13 @@
-cd workspace
-dotnet run --project ..\..\src\DynamicCheck.csproj
+@echo off
+
+SET SCRIPTPATH=%~dp0
+SET WORKSPACE=../workspace
+PROJECT_FILE=../src/DynamicCheck.csproj
+
+cd %SCRIPTPATH%
+
+mkdir %WORKSPACE%
+
+cd %WORKSPACE%
+
+dotnet run -c release --project %PROJECT_FILE%
